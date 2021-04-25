205e53b2686b8fe9ca42b036da4a268df15e35f6
Alex Dudley <alexdudley@me.com>
2021-04-24 21:08:44 -0500

    Add: When a new user creates an account, it logs them in"

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './NavBar.css'
@@ -7,31 +7,56 @@ import logo from './logo.png'
 import { userStorageKey } from "../../auth/authSettings"
 
 const Logout = () => {
+
   if (window.confirm("Are you sure you want to log out?")) {
     sessionStorage.setItem(userStorageKey, "")
   }
 }
 
-export const NavBar = (props) => {
+
+export const NavBar = () => {
+  const [ isAuthenticated, setIsAuthenticated ] = useState(false)
+  const currentUser = sessionStorage.getItem('user_id')
+
+  const userAuthenticated = () => {
+    if (currentUser !== '') {
+      setIsAuthenticated(true)
+    }
+  }
+
+  useEffect(() => {
+    userAuthenticated()
+  }, [])
+
   return (
     <nav className="navbar">
       <img src={ logo } alt='logo' />
 
       <ul className="nav nav-pills nav-fill">
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Dashboard</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/social">Social</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/habits">My Habits</Link>
-        </li>
+        { isAuthenticated
+          ?
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Dashboard</Link>
+          </li>
+          : null }
+        { isAuthenticated
+          ? <li className="nav-item">
+            <Link className="nav-link" to="/social">Social</Link>
+          </li>
+          : null }
+        { isAuthenticated
+          ? <li className="nav-item">
+            <Link className="nav-link" to="/habits">My Habits</Link>
+          </li>
+          : null }
         <li className="nav-item">
           <Link className="nav-link" to="/about">About</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="" onClick={ Logout }>Logout</Link>        </li>
+        { isAuthenticated
+          ? <li className="nav-item">
+            <Link className="nav-link" to="" onClick={ Logout }>Logout</Link>
+          </li>
+          : null }
       </ul>
     </nav>
   )
