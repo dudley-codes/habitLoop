27a64234b981f6c40e21ee86501f18635927701f
Alex Dudley <alexdudley@me.com>
2021-04-27 11:55:38 -0500

    breaking for lunch

--- src/components/data/HabitForm.js
+++ src/components/data/HabitForm.js
@@ -28,13 +28,13 @@ export const NewHabit = () => {
     e.preventDefault();
     setIsLoading(true);
 
-    const habit = habit.habit;
-    const userId = currentUserId;
-    const habitStart = Date.now();
-    const goodHabit = true;
-    const cue = habit.cue;
-    const reward = habit.reward;
-    const frequency = habit.frequency
+    // const habit = habit.habit;
+    // const userId = currentUserId;
+    // const habitStart = Date.now();
+    // const goodHabit = true;
+    // const cue = habit.cue;
+    // const reward = habit.reward;
+    // const frequency = habit.frequency
 
     setIsLoading(true);
     addHabit(habit)
@@ -50,9 +50,27 @@ export const NewHabit = () => {
         <div className='habit-form__group'>
           <label htmlFor='habit'>Habit:</label>
           <input type='text' id='habit' onChange={ handleControlledInputChange } required autoFocus className='form-control' placeholder='e.g. Work Out' value={ habit.habit } />
-
         </div>
       </fieldset>
+      <fieldset>
+        <div className='habit-form__group'>
+          <label htmlFor='cue'>Cue:</label>
+          <input type='text' id='cue' onChange={ handleControlledInputChange } required autoFocus className='form-control' placeholder='e.g. after waking up' value={ habit.cue } />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className='habit-form__group'>
+          <label htmlFor='reward'>Reward:</label>
+          <input type='text' id='reward' onChange={ handleControlledInputChange } required autoFocus className='form-control' placeholder='e.g. endorphins from working out' value={ habit.reward } />
+        </div>
+      </fieldset>
+      <fieldset>
+        <div className='habit-form__group'>
+          <label htmlFor='frequency'>frequency:</label>
+          <input type='text' id='frequency' onChange={ handleControlledInputChange } required autoFocus className='form-control' placeholder='e.g. 5' value={ habit.frequency } /> <label htmlFor='frequency'>x per week</label>
+        </div>
+      </fieldset>
+      <button className='btn btn-primary' type='button' disabled={ isLoading } onClick={ handleClickSaveHabit }>Save Habit</button>
     </form>
   )
 }
