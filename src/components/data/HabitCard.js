5fc600f4e1cdee7b721b92db72d386889165102a
Alex Dudley <alexdudley@me.com>
2021-04-28 13:27:21 -0500

    Add: bad habits now show up in red and start at 100%

--- src/components/data/HabitCard.js
+++ src/components/data/HabitCard.js
@@ -3,43 +3,74 @@ import editIcon from './images/edit-icon.svg';
 import plusIcon from './images/plus.svg';
 import minusIcon from './images/minus.svg';
 import './Habit.css'
-import { getCurrentMonth, getCurrentYear } from '../../modules/helpers';
+import { getCurrentMonth, getCurrentYear, daysInMonth } from '../../modules/helpers';
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
-
+console.log('days', daysInMonth)
 
 export const HabitCard = ({ habit }) => {
-
-  console.log('habit count', habit.count)
-
+  const goodHabit = habit.goodHabit
   const habitGoal = Math.floor((habit.count.length) / (habit.frequency * 4) * 100)
 
-  console.log('habitGoal', habitGoal)
+  const badHabitFreq = () => {
+    const habitDays = habit.frequency * daysInMonth
+    const habitTotal = Math.floor(((habitDays - habit.count.length) / habitDays) * 100)
+
+    return habitTotal
+  }
+
+  console.log('habitGoal', badHabitFreq())
 
   return (
-    <>
-      <div className='habit--card'>
-        <div className='habit--card__details'>
-          <div className='habit--card__habit'>
-            <div>{ habit.habit }</div>
-            <img src={ editIcon } alt='edit icon' className='edit--icon' />
+    goodHabit ?
+      <>
+        <div className='habit--card'>
+          <div className='habit--card__details'>
+            <div className='habit--card__habit'>
+              <div>{ habit.habit }</div>
+              <img src={ editIcon } alt='edit icon' className='edit--icon' />
+            </div>
+            <div className='habit--card__percent'>{ habitGoal }%</div>
           </div>
-          <div className='habit--card__percent'>{ habitGoal }%</div>
-        </div>
-        <div className='habit--progress__cont'>
-          <div className='habit--progress'>
-            <div>
-              <ProgressBar now={ habitGoal } variant='success' />
+          <div className='habit--progress__cont'>
+            <div className='habit--progress'>
+              <div>
+                <ProgressBar now={ habitGoal } variant='success' />
+              </div>
+            </div>
+            <div className='habit--plus'>
+              <img src={ plusIcon } alt='add to habit icon' />
+            </div>
+            <div className='habit--minus'>
+              <img src={ minusIcon } alt='subtract from habit icon' />
             </div>
           </div>
-          <div className='habit--plus'>
-            <img src={ plusIcon } alt='add to habit icon' />
+        </div>
+      </>
+      :
+      <>
+        <div className='habit--card'>
+          <div className='habit--card__details'>
+            <div className='habit--card__habit'>
+              <div>{ habit.habit }</div>
+              <img src={ editIcon } alt='edit icon' className='edit--icon' />
+            </div>
+            <div className='habit--card__percent'>{ badHabitFreq() }%</div>
           </div>
-          <div className='habit--minus'>
-            <img src={ minusIcon } alt='subtract from habit icon' />
+          <div className='habit--progress__cont'>
+            <div className='habit--progress'>
+              <div>
+                <ProgressBar now={ badHabitFreq() } variant='danger' />
+              </div>
+            </div>
+            <div className='habit--plus'>
+              <img src={ plusIcon } alt='add to habit icon' />
+            </div>
+            <div className='habit--minus'>
+              <img src={ minusIcon } alt='subtract from habit icon' />
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
   )
 }
