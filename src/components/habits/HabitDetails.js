7f3f8322ecea4f28606adca631cf53c4129b8edc
Alex Dudley <alexdudley@me.com>
2021-05-03 13:16:49 -0500

    Add: Beginning details cards show on DOM

--- src/components/habits/HabitDetails.js
+++ src/components/habits/HabitDetails.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getHabitsByUser } from '../../modules/HabitProvider';
+import { HabitDetailsCard } from './HabitDetailsCard';
+
 
 export const HabitDetails = () => {
+  const currentUserId = sessionStorage.getItem('user_id');
+  const [ habits, setHabits ] = useState([]);
+
+  // fetches all habits that belong to the current signed in user
+  const fetchHabits = () => {
+    const filterHabits = getHabitsByUser(currentUserId)
+      .then(res => setHabits(res))
+    return filterHabits
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+  // Returns habit cards for all user habits
   return (
     <>
-      <h3>Test</h3>
+      <section className='dashboard--container'>
+        <div className='habit--container'>
+          <h3>Details</h3>
+          { habits.map(habit =>
+            <HabitDetailsCard
+              key={ habit.id }
+              habit={ habit }
+              fetchHabits={ fetchHabits }
+            />
+          ) }
+        </div>
+      </section>
     </>)
 }
---
+++ src/components/habits/HabitDetailsCard.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import './Habit.css'
+import { getCurrentMonth, getCurrentYear, daysInMonth } from '../../modules/helpers';
+import ProgressBar from 'react-bootstrap/ProgressBar'
+import { HabitEditModal } from './HabitEditModal';
+import { IncreaseCount } from './HabitCount';
+import { Redirect } from 'react-router';
+
+export const HabitDetailsCard = ({ habit, fetchHabits }) => {
+
+  const goodHabit = habit.goodHabit
+
+  // Fetches habits and then creats a new array with only habit counts from the current month.
+  const filterHabits = () => {
+    const filter = habit.count.filter(count => {
+      let date = new Date(count.date)
+      if (date.getMonth() === getCurrentMonth() &
+        date.getFullYear() === getCurrentYear() || count.length === 0) {
+
+        return count
+      }
+    })
+    return filter.length
+  }
+
+  // Takes habit.frequency (weekly habit goal) and divides by 7. This gives us a daily average number that we multiply times the 
+  // number of days in the month. We then divide our current monthly count by this number and multiply times 100 to give us the 
+  // current progress towards a particular goal.
+  const monthlyPercentage = Math.floor((filterHabits()) / (habit.frequency / 7 * daysInMonth) * 100)
+
+  // Calculates the bad habit frequency in the same manner as above. In a bad habit, habit.frequency represents 
+  // a users average habit count per week. Again, we divide this number by 7 and multiply times the number of days in the month
+  // in order to get our monthly total. Because NOT doing the habit means success, we subtract current habit.count.length from
+  // this number. Meaning we start at 100% and every time the habit is performed, the user approaches zero. 
+  const badHabitFreq = () => {
+    const habitDays = habit.frequency / 7 * daysInMonth
+    const habitTotal = Math.floor(((habitDays - filterHabits()) / habitDays) * 100)
+
+    return habitTotal
+  }
+
+  const progStyle = {
+    height: '30px',
+    color: 'red'
+  }
+
+  // If progress in bad habit is over 50%, set to good color, if between 
+  // 25% and 50%, set to yellow. Below 50%, set to red.
+  const goodOrBadProg = () => {
+    if (badHabitFreq() >= 75) {
+      return 'good'
+    } else if (badHabitFreq() >= 25 & badHabitFreq() < 75) {
+      return 'warning'
+    } else {
+      return 'danger'
+    }
+  }
+
+  // returns the habit card that contains the habit name, current progress and allows the user to increase or decrease a habit count.
+  return (
+    goodHabit ?
+      <>
+        <div className='habit--card'>
+          <div className='habit--card__outer'>
+            <div className='habit--card__details'>
+              <div className='habit--card__habit'>
+                <div>{ habit.habit }</div>
+                <HabitEditModal
+                  habitId={ habit.id }
+                  fetchHabits={ fetchHabits }
+                />
+              </div>
+            </div>
+            <div className='habit--progress__cont'>
+              <div className='habit--progress'>
+
+                <div>
+                  <ProgressBar now={ monthlyPercentage } variant='good' style={ progStyle } />
+                </div>
+              </div>
+            </div>
+          </div>
+          <IncreaseCount
+            habit={ habit }
+            fetchHabits={ fetchHabits }
+            filterHabits={ filterHabits } />
+        </div>
+      </>
+      :
+      <>
+        <div className='habit--card'>
+          <div className='habit--card__outer'>
+
+            <div className='habit--card__details'>
+              <div className='habit--card__habit'>
+                <div>{ habit.habit }</div>
+                <HabitEditModal
+                  habitId={ habit.id }
+                  fetchHabits={ fetchHabits }
+                />
+              </div>
+            </div>
+            <div className='habit--progress__cont'>
+              <div className='habit--progress'>
+                <div>
+                  <ProgressBar now={ badHabitFreq() } style={ progStyle } variant={ goodOrBadProg() } />
+                </div>
+              </div>
+            </div>
+          </div>
+          <IncreaseCount
+            habit={ habit }
+            fetchHabits={ fetchHabits }
+            filterHabits={ filterHabits } />
+        </div>
+      </>
+  )
+}
