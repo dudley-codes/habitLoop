d8cc9d6cf432906df0c8b149f3aacb7d10b4b618
Alex Dudley <alexdudley@me.com>
2021-05-11 15:02:59 -0500

    Add: Materialize CSS Framework installed

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './NavBar.css'
 // import 'papercss/dist/paper.css'
+import 'materialize-css/dist/css/materialize.min.css'
 import logo from './logo.png'
 import { userStorageKey } from "../../auth/authSettings"
 
