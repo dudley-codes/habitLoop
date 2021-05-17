cd73a356bf0968176aff3161ecdbe493a8e0ea65
Alex Dudley <alexdudley@me.com>
2021-05-17 10:43:55 -0500

    Minor adjustments to CSS

--- src/components/habits/HabitList.js
+++ src/components/habits/HabitList.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { HabitCard } from './HabitCard'
 import './Habit.css'
 import { getHabitsByUser } from '../../modules/HabitProvider';
-import { HabitDetailsCard } from './HabitDetailsCard';
 import { JournalList } from '../journal/JournalList';
 import { getAllUsers } from '../../modules/UserDataManager';
 import { NavBar } from '../nav/NavBar';
@@ -44,38 +43,38 @@ export const HabitList = () => {
     getCurrentUser()
   }, [])
   // Renders habit card
-  const RenderProgress = () => {
-    return (
-      <div className='habit-cont__list'>
-        { habits.map(habit =>
-          <HabitCard
-            key={ habit.id }
-            habit={ habit }
-            fetchHabits={ fetchHabits }
-          />
-        ) }
-      </div>
-    )
-  }
+  // const RenderProgress = () => {
+  //   return (
+  //     <div className='habit-cont__list'>
+  //       { habits.map(habit =>
+  //         <HabitCard
+  //           key={ habit.id }
+  //           habit={ habit }
+  //           fetchHabits={ fetchHabits }
+  //         />
+  //       ) }
+  //     </div>
+  //   )
+  // }
   // Renders journal list
-  const RenderJournal = () => {
-    return (
-      <JournalList
-        fetchHabits={ fetchHabits }
-        habitList={ habitList } />
-    )
-  }
+  // const RenderJournal = () => {
+  //   return (
+  //     <JournalList
+  //       fetchHabits={ fetchHabits }
+  //       habitList={ habitList } />
+  //   )
+  // }
   // Switch statement for which card to render
-  const RenderScreen = () => {
-    switch (navBar) {
-      case 0:
-        return <RenderProgress />
-        break;
-      case 1:
-        return <RenderJournal />
+  // const RenderScreen = () => {
+  //   switch (navBar) {
+  //     case 0:
+  //       return <RenderProgress />
+  //       break;
+  //     case 1:
+  //       return <RenderJournal />
 
-    }
-  }
+  //   }
+  // }
 
   // Returns habit cards for all user habits
   return (
@@ -98,7 +97,9 @@ export const HabitList = () => {
                   />
                 ) }
               </div>
-              <RenderJournal />
+              <JournalList
+                fetchHabits={ fetchHabits }
+                habitList={ habitList } />
             </div>
           </div>
         </div>
